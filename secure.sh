ca939fbc254ffa58cc1a7d3b002fe61d367762b9
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-03-24 20:15:58 -0400

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -4,7 +4,7 @@ echo "Select what you would like to do:
         1) Install ssh key
         2) Remove password login
         3) Change ssh port
-        4) Install 2FA for password login (Google Authenticator)"
+        4) Install 2FA with password login (Google Authenticator)"
 
 read -r ans
 if [ "$ans" = "1" ]; then
@@ -14,21 +14,19 @@ if [ "$ans" = "1" ]; then
                 3) File location"
         read -r ans
         if [ "$ans" = "1" ]; then
-               echo "Paste in the remote location of file (wget)"
+               echo "put in the remote location of file (wget)"
                read -r ans
-               if ! command -v wget &> /dev/null; then
-                        apt install wget ca-certificates -y &> /dev/null
-                        yum install wget ca-certificates -y &> /dev/null
+               if ! command -v wget &> /dev/null || ! command -v update-ca-certificates &> /dev/null; then
+                        (! command -v apt &> /dev/null) || (apt update && apt upgrade -y && apt install wget ca-certificates -y)
+                        (! command -v yum &> /dev/null) || (yum update -y && yum install wget ca-certificates -y)
                fi
-               wget -O tempkey "$ans"
-               holdkey=$(cat tempkey)
-               rm tempkey
+               holdkey=$(wget -qO- "$ans")
         elif [ "$ans" = "2" ]; then
                 echo "Paste in your public key"
                 read -r ans
                 holdkey="$ans"
         elif [ "$ans" = "3" ]; then
-                echo "Paste in the file with your public key"
+                echo "Put in the location of the file with your public key"
                 read -r ans
                 holdkey=$(cat "$ans")
         fi
@@ -52,7 +50,7 @@ Do you want to add this key? y/n"
                 fi
 
         else
-                echo "This key is already there, nothing added."
+                echo "This key is already there, nothing added"
         fi
 elif [ "$ans" = "2" ]; then
         echo "Do you want to remove password access y/n"
@@ -60,6 +58,8 @@ elif [ "$ans" = "2" ]; then
         if [ "$ans" = "y" ]; then
                 sed -i '/PasswordAuthentication /c\PasswordAuthentication no' /etc/ssh/sshd_config
                 echo "Password authentication has been removed"
+        else
+                echo "Nothing changed"
         fi
 
 elif [ "$ans" = "3" ]; then
@@ -68,16 +68,20 @@ elif [ "$ans" = "3" ]; then
         if [ "$ans" != "" ]; then
                 sed -i "/Port /c\Port $ans" /etc/ssh/sshd_config
                 echo "ssh port set to $ans"
+        else
+                echo "Nothing changed"
         fi
 elif [ "$ans" = "4" ]; then
         if ! command -v google-authenticator &> /dev/null; then
-                        apt install libpam-google-authenticator -y &> /dev/null
-                        yum install google-authenticator -y &> /dev/null
+                        (! command -v apt &> /dev/null) || (apt update && apt upgrade -y && apt install libpam-google-authenticator -y)
+                        (! command -v yum &> /dev/null) || (yum update -y && yum install epel-release -y && yum update -y && yum install google-authenticator -y)
         fi
         google-authenticator -t -d -f --rate-limit=3 --rate-time=30 --window-size=3
         sed -i '/ChallengeResponseAuthentication /c\ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
         if [ "$(grep "auth required pam_google_authenticator.so" /etc/pam.d/sshd)" = "" ]; then
                 echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd
         fi
+        echo "Please copy down the emergency codes"
 fi
-systemctl restart sshd
+service sshd restart
+echo "Test login with another terminal window before logging out"
